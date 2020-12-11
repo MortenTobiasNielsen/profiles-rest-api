6f024b79f3c3bd1ef3bb71d190a7a1ac34219e27
Morten <mtnielsen89@gmail.com>
2020-12-11 10:39:11 +0100

    Added allowed hosts

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = 'dja2p=5kvlz152fu8vfqwzoej!d!@ur!fl*7--!*wtf$thwyod'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get("DEBUG", 1)))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "ec2-54-93-229-65.eu-central-1.compute.amazonaws.com",
+    "127.0.0.1"
+]
 
 
 # Application definition
